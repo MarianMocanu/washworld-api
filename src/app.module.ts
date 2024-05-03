e4ac9ed81d945144681f3932a038bd49e8b84ef7
Marian <mocanumarian1993@gmail.com>
2024-05-03 16:12:04 +0200

    updated prettier conf

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { LocationModule } from './location/location.module';
 import { ServiceModule } from './service/service.module';
 import { StepsModule } from './steps/steps.module';
+import { LevelsModule } from './levels/levels.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(dbConfig), UserModule, LocationModule, ServiceModule, StepsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(dbConfig), UserModule, LocationModule, ServiceModule, StepsModule, LevelsModule],
   controllers: [],
 })
 export class AppModule {}
