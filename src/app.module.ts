f8370163b527f2471ce99145ca854974a791a6bc
Marian <mocanumarian1993@gmail.com>
2024-05-03 14:27:57 +0200

    adding dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from 'data.source';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(dbConfig)],
+  controllers: [],
 })
 export class AppModule {}
