0b039ca21fa936c80cf81a69c881e1c47e3e021d
Fryderyk Boncler <f.relcnob@gmail.com>
2024-05-12 16:11:04 +0200

    feat: added auth endpoints

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { AuthGuard, RequestWithUser } from './auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  @HttpCode(201)
+  signup(@Body() body: CreateUserDto) {
+    return this.authService.signup(body);
+  }
+
+  @Post('login')
+  @HttpCode(200)
+  login(@Body() body: LoginDto) {
+    return this.authService.login(body);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('verify')
+  @HttpCode(200)
+  validateToken(@Req() request: RequestWithUser) {
+    return { user: request.user, token: this.authService.refreshToken(request.user) };
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/login.dto';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import * as jwt from 'jsonwebtoken';
+import { User } from 'src/user/entities/user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async signup(signupDTO: CreateUserDto): Promise<User> {
+    return this.userService.create(signupDTO);
+  }
+
+  async login({ email, password }: LoginDto): Promise<{ user: User; token: string }> {
+    const foundUser = await this.userService.findOneByEmail(email);
+    if (!foundUser || !foundUser.comparePassword(password)) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    //   check for inactive account (instead of deleting account Admin should be able to deactivate it)
+    if (!foundUser.isActive) {
+      throw new UnauthorizedException('Inactive account');
+    }
+    const payload = {
+      email: foundUser.email,
+      id: foundUser.id,
+      fistName: foundUser.firstName,
+      lastName: foundUser.lastName,
+      role: foundUser.role,
+    };
+    return {
+      user: foundUser,
+      token: jwt.sign(payload, process.env.JWT_SECRET),
+    };
+  }
+
+  refreshToken(user: User): string {
+    return jwt.sign(user, process.env.JWT_SECRET);
+  }
+}
