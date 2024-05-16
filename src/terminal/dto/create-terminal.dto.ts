a958383074c98c41e88303031960afb818f6781f
Fryderyk Boncler <f.relcnob@gmail.com>
2024-05-16 21:09:44 +0200

    feat: added DTOs

--- src/terminal/dto/create-terminal.dto.ts
+++ src/terminal/dto/create-terminal.dto.ts
@@ -1,1 +1,24 @@
-export class CreateTerminalDto {}
+import { IsArray, IsEnum, IsNotEmpty } from 'class-validator';
+import { Location } from 'src/location/entities/location.entity';
+import { TerminalStatus } from '../entities/terminal.entity';
+import { Event } from 'src/event/entities/event.entity';
+export class CreateTerminalDto {
+  constructor(status: TerminalStatus, location: Location, events: Event[]) {
+    this.status = status;
+    this.location = location;
+    this.events = events;
+  }
+
+  @IsNotEmpty()
+  @IsEnum(TerminalStatus)
+  status: TerminalStatus;
+
+  @IsNotEmpty()
+  @IsEnum(Location)
+  location: Location;
+
+  @IsNotEmpty()
+  @IsArray()
+  @IsEnum(Event, { each: true })
+  events: Event[];
+}
--- src/terminal/dto/update-terminal.dto.ts
+++ src/terminal/dto/update-terminal.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTerminalDto } from './create-terminal.dto';
-
-export class UpdateTerminalDto extends PartialType(CreateTerminalDto) {}
+import { Location } from 'src/location/entities/location.entity';
+import { TerminalStatus } from '../entities/terminal.entity';
+import { Event } from 'src/event/entities/event.entity';
+export class UpdateTerminalDto extends PartialType(CreateTerminalDto) {
+  constructor(status: TerminalStatus, location: Location, events: Event[]) {
+    super();
+    this.status = status;
+    this.location = location;
+    this.events = events;
+  }
+}
