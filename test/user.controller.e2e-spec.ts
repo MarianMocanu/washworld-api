0c9a561d5b4871da266d9d7461568751a42faa13
ThordurTh <thordur921@gmail.com>
2024-05-30 14:37:26 +0200

    User tests pass

--- test/user.controller.e2e-spec.ts
+++ test/user.controller.e2e-spec.ts
@@ -95,71 +95,73 @@ describe('User Controller (e2e)', () => {
     });
   });
 
-  // describe('/user/:id (PATCH)', () => {
-  //   it('should return 400 status code if user id is not a number', async () => {
-  //     const response = await request(app.getHttpServer())
-  //       .patch('/user/a9999999999')
-  //       .set('Authorization', token);
-  //     expect(response.statusCode).toBe(400);
-  //     expect(response.body.message).toBe('User id is not a number');
-  //   });
+  describe('/user/:id (PATCH)', () => {
+    it('should return 400 status code if user id is not a number', async () => {
+      const response = await request(app.getHttpServer())
+        .patch('/user/a9999999999')
+        .set('Authorization', token);
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe('User id is not a number');
+    });
 
-  //   it('should return 404 if user not found', async () => {
-  //     const response = await request(app.getHttpServer())
-  //       .patch('/user/999999')
-  //       .set('Authorization', token);
-  //     expect(response.statusCode).toBe(404);
-  //     expect(response.body.message).toBe('User not found');
-  //   });
+    it('should return 404 if user not found', async () => {
+      const response = await request(app.getHttpServer())
+        .patch('/user/999999')
+        .set('Authorization', token);
+      expect(response.statusCode).toBe(404);
+      expect(response.body.message).toBe('User not found');
+    });
 
-  //   it('should return 400 status code if invalid data', async () => {
-  //     const newUserDTO = new CreateUserDto('John', 'Doe', 'testemail@john.com', 'qwerty');
-  //     const newUser = await userService.create(newUserDTO);
-  //     const invalidUser = { ...newUser, firstName: 1234 };
+    it('should return 400 status code if invalid data', async () => {
+      const newUserDTO = new CreateUserDto('John', 'Doe', 'testemail@john.com', 'qwerty');
+      const newUser = await userService.create(newUserDTO);
+      const invalidUser = { ...newUser, firstName: 1234 };
 
-  //     const response = await request(app.getHttpServer())
-  //       .patch(`/user/${invalidUser.id}`)
-  //       .set('Authorization', token)
-  //       .send(invalidUser);
+      const response = await request(app.getHttpServer())
+        .patch(`/user/${invalidUser.id}`)
+        .set('Authorization', token)
+        .send(invalidUser);
 
-  //     expect(response.statusCode).toBe(400);
+      expect(response.statusCode).toBe(400);
 
-  //     await userService.remove(newUser.id);
-  //   });
+      await userService.remove(newUser.id);
+    });
 
-  //   it('should return 200 and the updated data', async () => {
-  //     const newUserDTO = new CreateUserDto('John', 'Doe', 'testemail@john.com', 'qwerty');
-  //     const newUser = await userService.create(newUserDTO);
-  //     const updatedUser = { ...newUser, firstName: 'updated firstName' };
+    it('should return 200 and the updated data', async () => {
+      const newUserDTO = new CreateUserDto('John', 'Doe', 'testemail@john.com', 'qwerty');
+      const newUser = await userService.create(newUserDTO);
+      const updatedUser = { ...newUser, firstName: 'updated firstName' };
 
-  //     const response = await request(app.getHttpServer())
-  //       .patch(`/user/${updatedUser.id}`)
-  //       .set('Authorization', token)
-  //       .send(updatedUser);
+      const response = await request(app.getHttpServer())
+        .patch(`/user/${updatedUser.id}`)
+        .set('Authorization', token)
+        .send(updatedUser);
 
-  //     expect(response.statusCode).toBe(200);
-  //     expect(response.body).toBeDefined();
-  //     expect(response.body.firstName).toBe('Updated Firstname');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toBeDefined();
+      expect(response.body.firstName).toBe('Updated Firstname');
 
-  //     await userService.remove(newUser.id);
-  //   });
-  // });
+      await userService.remove(newUser.id);
+    });
+  });
 
-  // describe('/user/:id (DELETE)', () => {
-  //   it('should return 400 status code if user id is not a number', async () => {
-  //     const response = await request(app.getHttpServer())
-  //       .delete('/user/a123')
-  //       .set('Authorization', token);
-  //     expect(response.statusCode).toBe(400);
-  //     expect(response.body.message).toBe('User id is not a number');
-  //   });
+  describe('/user/:id (DELETE)', () => {
+    it('should return 400 status code if user id is not a number', async () => {
+      const response = await request(app.getHttpServer())
+        .delete('/user/a123')
+        .set('Authorization', token);
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe('User id is not a number');
+    });
 
-  //   it('should return 404 if user not found', async () => {
-  //     const response = await request(app.getHttpServer()).delete('/user/999999');
-  //     expect(response.statusCode).toBe(404);
-  //     expect(response.body.message).toBe('User not found');
-  //   });
-  // });
+    it('should return 404 if user not found', async () => {
+      const response = await request(app.getHttpServer())
+        .delete('/user/999999')
+        .set('Authorization', token);
+      expect(response.statusCode).toBe(404);
+      expect(response.body.message).toBe('User not found');
+    });
+  });
 
   afterAll(async () => {
     await userService.remove(user.id);
