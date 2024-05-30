a33321f00d3974e95c3aeca3d0a8e8719036864e
ThordurTh <thordur921@gmail.com>
2024-05-30 15:16:40 +0200

    locations tests pass

--- test/locations.controller.e2e-spec.ts
+++ test/locations.controller.e2e-spec.ts
@@ -2,12 +2,21 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from 'src/app.module';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from 'src/auth/auth.service';
+import { User } from 'src/user/entities/user.entity';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { LoginDto } from 'src/auth/dto/login.dto';
+import { LocationStatus } from 'src/locations/entities/location.entity';
 import { LocationsService } from 'src/locations/locations.service';
 import { CreateLocationDto } from 'src/locations/dto/create-location.dto';
-import { LocationStatus } from 'src/locations/entities/location.entity';
 
 describe('Location Controller (e2e)', () => {
   let app: INestApplication;
+  let userService: UserService;
+  let authService: AuthService;
+  let token: string;
+  let user: User;
   let locationsService: LocationsService;
 
   beforeAll(async () => {
@@ -15,99 +24,124 @@ describe('Location Controller (e2e)', () => {
       imports: [AppModule],
     }).compile();
 
+    userService = moduleFixture.get<UserService>(UserService);
+    authService = moduleFixture.get<AuthService>(AuthService);
     locationsService = moduleFixture.get<LocationsService>(LocationsService);
+
+    // Sign up
+    const userDTO = new CreateUserDto('test', 'user', 'testuser@mail.com', '12345');
+    user = await userService.create(userDTO);
+
+    // Log in
+    const loginDTO = new LoginDto('test4@mail.com', '12345');
+    const loginResponse = await authService.login(loginDTO);
+    token = loginResponse.token;
+
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
   // FIXME SEND JWT TOKEN
-  // describe('/locations (GET)', () => {
-  //   it('should return 200 and an array of locations', async () => {
-  //     const response = await request(app.getHttpServer()).get('/locations');
-  //     expect(response.statusCode).toBe(200);
-  //     expect(response.body).toBeDefined();
-  //     expect(Array.isArray(response.body)).toBe(true);
-  //   });
-  // });
+  describe('/locations (GET)', () => {
+    it('should return 200 and an array of locations', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/locations')
+        .set('Authorization', token);
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toBeDefined();
+      expect(Array.isArray(response.body)).toBe(true);
+    });
+  });
 
-  // describe('/locations/id (GET)', () => {
-  //   it('should return a location based on its id', async () => {
-  //     const newlocation = new CreateLocationDto({
-  //       city: 'København',
-  //       streetName: 'Nørrebrogade',
-  //       streetNumber: '56',
-  //       postalCode: '2200',
-  //       openingHours: {
-  //         monday: { from: '00:00', to: '24:00' },
-  //         tuesday: { from: '00:00', to: '24:00' },
-  //         wednesday: { from: '00:00', to: '24:00' },
-  //         thursday: { from: '00:00', to: '24:00' },
-  //         friday: { from: '00:00', to: '24:00' },
-  //         saturday: { from: '00:00', to: '24:00' },
-  //         sunday: { from: 'Closed', to: 'Closed' },
-  //       },
-  //       status: LocationStatus.closed,
-  //       image: 'https://example.com/image3.jpg',
-  //       coordinates: {
-  //         latitude: 55.6887,
-  //         longitude: 12.5489,
-  //       },
-  //     });
+  describe('/locations/id (GET)', () => {
+    it('should return a location based on its id', async () => {
+      const newLocation = new CreateLocationDto(
+        'København',
+        'Nørrebrogade',
+        '56',
+        '2200',
+        {
+          monday: { from: '00:00', to: '24:00' },
+          tuesday: { from: '00:00', to: '24:00' },
+          wednesday: { from: '00:00', to: '24:00' },
+          thursday: { from: '00:00', to: '24:00' },
+          friday: { from: '00:00', to: '24:00' },
+          saturday: { from: '00:00', to: '24:00' },
+          sunday: { from: 'Closed', to: 'Closed' },
+        },
+        LocationStatus.closed,
+        'https://example.com/image3.jpg',
+        {
+          latitude: 55.6887,
+          longitude: 12.5489,
+        },
+      );
 
-  //     const locationCreated = await locationsService.create(newlocation);
+      const locationCreated = await locationsService.create(newLocation);
 
-  //     const response = await request(app.getHttpServer()).get(`/locations/${locationCreated.id}`);
+      const response = await request(app.getHttpServer())
+        .get(`/locations/${locationCreated.id}`)
+        .set('Authorization', token);
 
-  //     expect(response.statusCode).toBe(200);
-  //     expect(response.body).toBeDefined();
-  //     expect(response.body.address).toBe('2200 København, Nørrebrogade 56');
-  //   });
-  // });
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toBeDefined();
+      expect(response.body.address).toBe('2200 København, Nørrebrogade 56');
 
-  // describe('/locations (POST)', () => {
-  //   it('should return 400 status code if invalid data', async () => {
-  //     const invalidLocation = { address: false };
-  //     const response = await request(app.getHttpServer()).post('/locations').send(invalidLocation);
-  //     expect(response.statusCode).toBe(400);
-  //   });
+      // delete the location at the end of the test
+      await locationsService.remove(response.body.id);
+    });
+  });
 
-  //   it('should return 201 and the new location after creation', async () => {
-  //     const newLocation = {
-  //       address: 'Nørrebrogade 56, 2200 København',
-  //       city: 'København',
-  //       streetName: 'Nørrebrogade',
-  //       streetNumber: '56',
-  //       postalCode: '2200',
-  //       openingHours: {
-  //         monday: { from: '00:00', to: '24:00' },
-  //         tuesday: { from: '00:00', to: '24:00' },
-  //         wednesday: { from: '00:00', to: '24:00' },
-  //         thursday: { from: '00:00', to: '24:00' },
-  //         friday: { from: '00:00', to: '24:00' },
-  //         saturday: { from: '00:00', to: '24:00' },
-  //         sunday: { from: 'Closed', to: 'Closed' },
-  //       },
-  //       status: 'closed',
-  //       image: 'https://example.com/image3.jpg',
-  //       coordinates: {
-  //         latitude: 55.6887,
-  //         longitude: 12.5489,
-  //       },
-  //     };
+  describe('/locations (POST)', () => {
+    it('should return 400 status code if invalid data', async () => {
+      const invalidLocation = { address: false };
+      const response = await request(app.getHttpServer())
+        .post('/locations')
+        .send(invalidLocation)
+        .set('Authorization', token);
+      expect(response.statusCode).toBe(400);
+    });
 
-  //     const response = await request(app.getHttpServer()).post('/locations').send(newLocation);
+    it('should return 201 and the new location after creation', async () => {
+      const newLocation = new CreateLocationDto(
+        'København',
+        'Nørrebrogade',
+        '56',
+        '2200',
+        {
+          monday: { from: '00:00', to: '24:00' },
+          tuesday: { from: '00:00', to: '24:00' },
+          wednesday: { from: '00:00', to: '24:00' },
+          thursday: { from: '00:00', to: '24:00' },
+          friday: { from: '00:00', to: '24:00' },
+          saturday: { from: '00:00', to: '24:00' },
+          sunday: { from: 'Closed', to: 'Closed' },
+        },
+        LocationStatus.closed,
+        'https://example.com/image3.jpg',
+        {
+          latitude: 55.6887,
+          longitude: 12.5489,
+        },
+      );
 
-  //     expect(response.statusCode).toBe(201);
-  //     expect(response.body).toBeDefined();
-  //     expect(response.body.id).toBeDefined();
+      const response = await request(app.getHttpServer())
+        .post('/locations')
+        .send(newLocation)
+        .set('Authorization', token);
 
-  //     // delete the location at the end of the test
-  //     await locationsService.remove(response.body.id);
-  //   });
-  // });
+      expect(response.statusCode).toBe(201);
+      expect(response.body).toBeDefined();
+      expect(response.body.id).toBeDefined();
 
-  // afterAll(async () => {
-  //   await app.close();
-  // });
+      // delete the location at the end of the test
+      await locationsService.remove(response.body.id);
+    });
+  });
+
+  afterAll(async () => {
+    await userService.remove(user.id);
+    await app.close();
+  });
 });
